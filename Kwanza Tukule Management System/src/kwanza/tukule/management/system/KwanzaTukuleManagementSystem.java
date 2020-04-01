2907cc044ff5979173e408c503b35c867844c944
Mariwa <samuelmariwa@gmail.com>
2020-04-01 17:47:17 -0400

    everything

---
+++ Kwanza Tukule Management System/src/kwanza/tukule/management/system/KwanzaTukuleManagementSystem.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package kwanza.tukule.management.system;
+
+import java.awt.Image;
+import java.awt.Toolkit;
+
+/**
+ *
+ * @author RS
+ */
+public class KwanzaTukuleManagementSystem {
+    
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Login lg = new Login();
+            lg.setVisible(true);      
+    }
+   
+}
