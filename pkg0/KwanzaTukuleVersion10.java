8ca547761450d76ec0a963f288450cd6466ecad9
Mariwa <samuelmariwa@gmail.com>
2020-04-23 22:51:16 -0400

    softward

---
+++ pkg0/KwanzaTukuleVersion10.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package kwanza.tukule.version.pkg1.pkg0;
+
+/**
+ *
+ * @author Mariwa
+ */
+public class KwanzaTukuleVersion10 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Login lg = new Login();
+        lg.setVisible(true);
+    }
+    
+}
